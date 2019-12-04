e0aafbae79215021b907603b35775b084988d5c9
Jorge Galvan <jorgegalvan@Jorges-MacBook-Air.local>
2019-12-04 11:27:43 -0600

    navigation between all hikes and new hike

--- controllers/hikes.js
+++ controllers/hikes.js
@@ -1,20 +1,31 @@
 var Hike = require("../models/user");
 
 module.exports = {
+  new: newHike,
   index,
   show,
   create,
   delete: deleteHike
 };
 
+function newHike(req, res) {
+  res.render("hikes/new");
+}
+
 function index(req, res) {
   Hike.find({}, function(err, hikes) {
-    res.render("hikes/index", { title: "All Hikes", hikes });
+    res.render("hikes/index", { hikes });
   });
 }
 
 function show(req, res) {}
 
-function create(req, res) {}
+function create(req, res) {
+  var hike = new Hike(req.body);
+  hike.save(function(err) {
+    if (err) return res.render("hikes/new");
+    res.redirect("/hikes");
+  });
+}
 
 function deleteHike(req, res) {}
--- routes/hikes.js
+++ routes/hikes.js
@@ -5,4 +5,10 @@ var hikesCtrl = require("../controllers/hikes");
 /* GET all hikes that are added */
 router.get("/", hikesCtrl.index);
 
+/*  a new hike */
+router.post("/", hikesCtrl.create);
+
+// CRUD-less route take to new page to create hike
+router.get("/new", hikesCtrl.new);
+
 module.exports = router;
